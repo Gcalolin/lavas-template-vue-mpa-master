ce7b2de486237e7c61b89b4d429a5ccbe6351b05
Gcalolin <418146654@qq.com>
2017-08-09 14:07:58 +0800

    first commit

---
+++ src/pages/member/Member.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>我的云购</div>
+</template>
+
+<script>
+    import { mapActions } from 'vuex';
+    
+    export default {
+        name: 'member',
+        props: {},
+        methods: {
+            ...mapActions('appShell/appBottomNavigator', [
+                'showBottomNav',
+                'activeBottomNav'
+            ])
+        },
+        activated() {
+            this.activeBottomNav('member');
+            this.showBottomNav();
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
---
+++ src/pages/product/Product.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>所有商品</div>
+</template>
+
+<script>
+    import { mapActions } from 'vuex';
+    
+    export default {
+        name: 'product',
+        props: {},
+        mounted() {
+        },
+        methods: {
+            ...mapActions('appShell/appBottomNavigator', [
+                'showBottomNav',
+                'activeBottomNav'
+            ])
+        },
+        activated() {
+            this.activeBottomNav('product');
+            this.showBottomNav();
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    
+</style>
